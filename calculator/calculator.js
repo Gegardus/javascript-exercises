fbba9a564b74fc41e7cefc213f770ac86873f685
Vahan <stgegard@gmail.com>
2021-07-27 01:33:39 +0400

    new ex

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,48 @@
-const add = function() {
-	
+const add = function(num_1, num_2) {
+       let result = num_1 + num_2;
+	return result;
 };
 
-const subtract = function() {
-	
+const subtract = function(num_1, num_2) {
+	let result = num_1 - num_2;
+	return result;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum = 0;
+	for(let i = 0; i < arr.length; i++) {
+      sum += arr[i];
+    }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+           let result = 1;
+     for(let i = 0; i < arr.length; i++) {
+            result *= arr[i];
+    }
+	return result;
 };
 
-const power = function() {
-	
+const power = function(num_1, num_2) {
+     let result = 1;
+   while(num_2 > 0) {
+           result *= num_1;
+     num_2--;
+    }
+	return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+    let fac = 1;
+    let arr = [];
+  	for(let i = 1; i <= num; i++) {
+            arr.push(i);
+    }
+  for(let i = 0; i < arr.length; i++) {
+            fac *= arr[i];
+    }	
+      return fac;
 };
 
 module.exports = {
--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,24 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  ns = [];
+  let alpha = "abcdefghijklmnoprstuvwxyz";  
+  let a = str.toLowerCase();  
 
+  for(let i = 0; i < a.length; i++) {
+       for(let j = 0; j < alpha.length; j++) {
+        if(a.charAt(i) === alpha.charAt(j)) {
+          ns.push(a.charAt(i));
+            }
+       }
+  }
+ 
+  let nString = ns.join("");
+  let rev = ns.reverse();
+  let revStr = rev.join("");
+
+   if( nString === revStr ) {
+     return true;
+    }  
+ return   false; //sa + "\n " + sb;
 };
 
 module.exports = palindromes;
