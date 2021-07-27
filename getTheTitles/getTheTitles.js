4ecac194ddeedc72ccb24be575995abd12a24a65
Vahan <stgegard@gmail.com>
2021-07-27 20:27:15 +0400

    Titles

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,16 @@
-const getTheTitles = function() {
+const getTheTitles = function(arr) {   
+       let titles = [];
+       for (let i = 0; i < arr.length; i++) {       
+         e = Object.entries(arr[i]);
+         e.forEach(([key, value]) => {
+         if (key === "title") {      
+           titles.push(value);
+       }
+         });
 
+
+      } 
+         return  titles;
 };
 
 module.exports = getTheTitles;
