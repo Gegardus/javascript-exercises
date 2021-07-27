197b80b222e725ea1ef16d0b91d13fa5ce20a168
Vahan <stgegard@gmail.com>
2021-07-27 13:09:34 +0400

    fibo

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+  
+    let fibo = [];
+  
+    fibo[0] = 1;
+    fibo[1] = 1;
 
+    if(num < 0) return "OOPS";
+  
+  for(let i = 2; i < num; i++) {    
+    fibo[i] = fibo[i -1] + fibo[i - 2]; 
+  }
+  
+return fibo.pop();
 };
 
 module.exports = fibonacci;
